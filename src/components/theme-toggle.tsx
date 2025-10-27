7a2fbf9ded2dcf86a2aff0885eabc8982892af65
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2025-10-27 15:05:17 +0100

    init commit

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import {Button} from "@/src/components/ui/button";
+
+export function ThemeToggle({ className }) {
+    const { theme, setTheme } = useTheme()
+
+    return (
+        <Button
+            className={className}
+            variant="outline"
+            size="icon"
+            aria-label="theme toggle"
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        >
+            <Sun className="size-5 dark:hidden" />
+            <Moon className="hidden size-5 dark:block" />
+        </Button>
+    )
+}
